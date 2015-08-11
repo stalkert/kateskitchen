80cb3c3d798bf2f426d791f6ea1cac5bce8c2236
stalkert <taranenko.yura@gmail.com>
2015-08-11 14:21:59 +0300

    v1.2
    second

--- wp-content/themes/twentyfifteen/index.php
+++ wp-content/themes/twentyfifteen/index.php
@@ -24,7 +24,8 @@ get_header(); ?>
 			<?php if ( is_home() && ! is_front_page() ) : ?>
 				<header>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-					<h2>Мои первый изменения через Git</h2>
+					<h2>ГЊГ®ГЁ ГЇГҐГ°ГўГ»Г© ГЁГ§Г¬ГҐГ­ГҐГ­ГЁГї Г·ГҐГ°ГҐГ§ Git</h2>
+					<h3>Second</h3>
 				</header>
 			<?php endif; ?>
 
