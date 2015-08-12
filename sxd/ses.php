f2d6e5576d5d0bf785dad31c5438cad0d4239c51
stalkert <taranenko.yura@gmail.com>
2015-08-12 21:33:08 +0300

    Add sxd

---
+++ sxd/ses.php
@@ -0,0 +1,31 @@
+<?php
+$SES = array (
+  '4253aeb6cb995b545fffbd5a265562bf' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'ru',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => 3306,
+      'my_user' => 'kateskit',
+      'my_pass' => 'Otpusk2015$',
+      'my_comp' => 0,
+      'my_db' => 'bubasik',
+      'auth' => 'mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '6',
+      'exitURL' => './',
+    ),
+    'time' => 1439404016,
+    'lng' => 'ru',
+  ),
+);
+?>
