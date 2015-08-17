5e72e4ca2c044870e16f143920d059642c7b28cf
stalkert <ITaranenko@ITaranenko.kiev.luxoft.com>
2015-08-17 15:42:11 +0300

    work 170715 add class active

---
+++ wp-content/themes/kateskitchen/js/myjs.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ITaranenko on 8/17/2015.
+ */
+$(document).ready(function(){
+    $("#navbar li").click(function(){
+        $("#navbar li").removeClass("active");
+        $(this).addClass("active");
+        $("#navbar li")
+    });
+});
