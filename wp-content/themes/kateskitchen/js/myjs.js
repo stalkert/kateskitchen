0bd7a3e1756deeef94219e34e31850fe33ce4fd5
stalkert <taranenko.yura@gmail.com>
2015-08-31 17:50:02 +0300

    work3108

--- wp-content/themes/kateskitchen/js/myjs.js
+++ wp-content/themes/kateskitchen/js/myjs.js
@@ -14,11 +14,20 @@ $(document).ready(function(){
         $("#navbar").addClass("nav-stacked");
     });
     $(window).on('load resize',windowSize);
+    $("#button_left_menu").click(function(){
+        $("#recepty").slideToggle(600);
+    });
     function windowSize(){
-        if($(window).width()>=768){
+        if($(window).width()>=750){
             $("#navbar").removeClass("nav-stacked");
+            $("#title_recepty").removeClass("dysplay_none");
+            $("#button_left_menu").addClass("dysplay_none");
+            $("#recepty").slideDown(1).removeClass("dysplay_none");
         }else {
             $("#navbar").addClass("nav-stacked");
+            $("#title_recepty").addClass("dysplay_none");
+            $("#button_left_menu").removeClass("dysplay_none");
+            $("#recepty").slideUp(1).addClass("dysplay_none");
         }
     }
 
