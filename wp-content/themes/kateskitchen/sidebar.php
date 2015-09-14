b39c46557c352236980ca02f19df8fab6120d742
stalkert <taranenko.yura@gmail.com>
2015-09-14 12:07:37 +0300

    work1409

--- wp-content/themes/kateskitchen/sidebar.php
+++ wp-content/themes/kateskitchen/sidebar.php
@@ -4,55 +4,55 @@
 
                     <h2 class="title1 red" id="title_recepty"><?php  echo get_the_title(18);?></h2>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(12); ?>"><?php echo get_cat_name(12); ?></a></li>
+                    <li><a href="<?php echo get_category_link(12); ?>"><?php echo get_cat_name(12); ?><?php $kateskit_category_count=get_category(12)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(14); ?>"><?php echo get_cat_name(14); ?></a></li>
+                    <li><a href="<?php echo get_category_link(14); ?>"><?php echo get_cat_name(14); ?><?php $kateskit_category_count=get_category(14)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(16); ?>"><?php echo get_cat_name(16); ?></a></li>
+                    <li><a href="<?php echo get_category_link(16); ?>"><?php echo get_cat_name(16); ?><?php $kateskit_category_count=get_category(16)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(18); ?>"><?php echo get_cat_name(18); ?></a></li>
+                    <li><a href="<?php echo get_category_link(18); ?>"><?php echo get_cat_name(18); ?><?php $kateskit_category_count=get_category(18)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(20); ?>"><?php echo get_cat_name(20); ?></a></li>
+                    <li><a href="<?php echo get_category_link(20); ?>"><?php echo get_cat_name(20); ?><?php $kateskit_category_count=get_category(20)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(22); ?>"><?php echo get_cat_name(22); ?></a></li>
+                    <li><a href="<?php echo get_category_link(22); ?>"><?php echo get_cat_name(22); ?><?php $kateskit_category_count=get_category(22)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(24); ?>"><?php echo get_cat_name(24); ?></a></li>
+                    <li><a href="<?php echo get_category_link(24); ?>"><?php echo get_cat_name(24); ?><?php $kateskit_category_count=get_category(24)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(26); ?>"><?php echo get_cat_name(26); ?></a></li>
+                    <li><a href="<?php echo get_category_link(26); ?>"><?php echo get_cat_name(26); ?><?php $kateskit_category_count=get_category(26)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(28); ?>"><?php echo get_cat_name(28); ?></a></li>
+                    <li><a href="<?php echo get_category_link(28); ?>"><?php echo get_cat_name(28); ?><?php $kateskit_category_count=get_category(28)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(30); ?>"><?php echo get_cat_name(30); ?></a></li>
+                    <li><a href="<?php echo get_category_link(30); ?>"><?php echo get_cat_name(30); ?><?php $kateskit_category_count=get_category(30)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(32); ?>"><?php echo get_cat_name(32); ?></a></li>
+                    <li><a href="<?php echo get_category_link(32); ?>"><?php echo get_cat_name(32); ?><?php $kateskit_category_count=get_category(32)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(34); ?>"><?php echo get_cat_name(34); ?></a></li>
+                    <li><a href="<?php echo get_category_link(34); ?>"><?php echo get_cat_name(34); ?><?php $kateskit_category_count=get_category(34)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(36); ?>"><?php echo get_cat_name(36); ?></a></li>
+                    <li><a href="<?php echo get_category_link(36); ?>"><?php echo get_cat_name(36); ?><?php $kateskit_category_count=get_category(36)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(38); ?>"><?php echo get_cat_name(38); ?></a></li>
+                    <li><a href="<?php echo get_category_link(38); ?>"><?php echo get_cat_name(38); ?><?php $kateskit_category_count=get_category(38)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(40); ?>"><?php echo get_cat_name(40); ?></a></li>
+                    <li><a href="<?php echo get_category_link(40); ?>"><?php echo get_cat_name(40); ?><?php $kateskit_category_count=get_category(40)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(42); ?>"><?php echo get_cat_name(42); ?></a></li>
+                    <li><a href="<?php echo get_category_link(42); ?>"><?php echo get_cat_name(42); ?><?php $kateskit_category_count=get_category(42)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
                     <li class="divider"></li>
                     <span class="glyphicon glyphicon-star-empty left_menu"></span>
-                    <li><a href="<?php echo get_category_link(44); ?>"><?php echo get_cat_name(44); ?></a></li>
+                    <li><a href="<?php echo get_category_link(44); ?>"><?php echo get_cat_name(44); ?><?php $kateskit_category_count=get_category(44)->category_count; if ($kateskit_category_count>0){echo "&nbsp(".$kateskit_category_count.")";}?></a></li>
 
                 </ul>
 
