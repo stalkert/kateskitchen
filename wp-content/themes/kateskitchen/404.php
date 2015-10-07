21a96e7822b2ae3c2ce8ca9f1b3a6a40d523ee7b
stalkert <taranenko.yura@gmail.com>
2015-10-07 15:07:43 +0300

    work0710

---
+++ wp-content/themes/kateskitchen/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?> 
+        <div class="row">
+            <?php get_sidebar();?>
+            <div class="col-md-6" id="content">
+             <div >
+			 <img id="error-404" src="<?php bloginfo( 'template_url' ); ?>/img/404error.jpg">
+			</div>			 
+
+					
+                    
+
+                
+                
+            </div>
+           <?php get_template_part( 'right-sidebar' ); ?>
+     <?php get_footer(); ?>    
--- wp-content/themes/kateskitchen/footer.php
+++ wp-content/themes/kateskitchen/footer.php
@@ -1,6 +1,6 @@
 <div class="row">
 			<div id="planet-earth" class="col-md-3">
-			<script type="text/javascript" src="//rf.revolvermaps.com/0/0/1.js?i=58bngdl4x1b&amp;s=220&amp;m=0&amp;v=false&amp;r=false&amp;b=000000&amp;n=false&amp;c=ff0000" async="async"></script>
+			<script type="text/javascript" src="//ra.revolvermaps.com/0/0/1.js?i=58bngdl4x1b&amp;s=220&amp;m=0&amp;v=false&amp;r=false&amp;b=000000&amp;n=false&amp;c=ff0000" async="async"></script>
 			</div>
             <div class="col-md-6" id="footer">
                 <a href="http://www.facebook.com/KateskitchenKatinaKuhnya" ><img src="<?php bloginfo( 'template_url' ); ?>/img/994.png" title="Я в Facebook!"></a>
